a5834849fe514beca8eec2569a4c8db186212f8b
nerv617 <nerv617@gmail.com>
2019-04-17 20:51:16 -0400

    updated progress

--- src/models/user.js
+++ src/models/user.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
-
-const User = mongoose.model("User", {
+const bcrypt = require("bcryptjs");
+const userSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
@@ -10,6 +10,7 @@ const User = mongoose.model("User", {
   email: {
     type: String,
     required: true,
+    unique: true,
     trim: true,
     lowercase: true,
     validate(enteredValue) {
@@ -34,5 +35,29 @@ const User = mongoose.model("User", {
     default: 0
   }
 });
+
+userSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new Error("Unable to login");
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    throw new Error("Unable to login");
+  }
+
+  return user;
+};
+
+//password hash
+userSchema.pre("save", async function(next) {
+  const user = this;
+  if (user.isModified("password")) {
+    user.password = await bcrypt.hash(user.password, 8);
+  }
+  next();
+});
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
