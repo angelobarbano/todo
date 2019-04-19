36d0ce12cef1d36533c9232df23fe2fd7228a480
nerv617 <nerv617@gmail.com>
2019-04-18 21:32:33 -0400

    authentication started

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -30,11 +32,27 @@ const userSchema = new mongoose.Schema({
       }
     }
   },
+  tokens: [
+    {
+      token: {
+        type: String,
+        required: true
+      }
+    }
+  ],
   age: {
     type: Number,
     default: 0
   }
 });
+
+userSchema.methods.generateAuthToken = async function() {
+  const user = this;
+  const token = jwt.sign({ _id: user._id.toString() }, "mySafeWordIsCacao");
+  user.tokens = user.tokens.concat({ token });
+  await user.save();
+  return token;
+};
 
 userSchema.statics.findByCredentials = async (email, password) => {
   const user = await User.findOne({ email });
--- src/routes/usersRoute.js
+++ src/routes/usersRoute.js
@@ -68,6 +68,7 @@ router.post("/users/login", async (req, res) => {
       req.body.email,
       req.body.password
     );
+    const token = await user.generateAuthToken();
     res.send(user);
   } catch (e) {
     res.status(400).send(e);
